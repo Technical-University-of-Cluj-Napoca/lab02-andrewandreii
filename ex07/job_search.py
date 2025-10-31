dea51c552015a2a705611bce3a8346ff7c388b18
andrewandreii <andrei3f@yahoo.com>
2025-10-31 18:12:26 +0200

    job_search: added argparse + bug fix

--- ex07/job_search.py
+++ ex07/job_search.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 from urllib.parse import quote_plus
 from datetime import timedelta, datetime
+from argparse import ArgumentParser
 
 from enum import Enum
 
@@ -55,7 +56,7 @@ def make_url(*, search_query: str = None, date: JuniorsRoDate = None, job_types:
         url += remote.to_param()
 
     if experience is not None:
-        url += JuniorsRoJobExperience.to_param(experience)
+        url += JuniorsRoExperience.to_param(experience)
 
     if search_query is not None:
         url += f"?q={quote_plus(search_query)}"
@@ -79,7 +80,12 @@ class JuniorsRoJob:
         return f"Job Title: {self.title}\n\tLocation: {self.location}\n\tTime Posted: {self.time_posted}\n\tTags: {', '.join(self.tags)}\n\tCompany: {self.company}"
 
 def approximate_time(text: str):
+    if " " not in text:
+        return None
     amount = int(text[:text.index(" ")])
+
+    if "ore" in text or "oră":
+        return timedelta(hours=amount)
 
     if "zile" in text or "zi" in text:
         return timedelta(days=amount)
@@ -95,13 +101,21 @@ def parse_html(html: str) -> (list[JuniorsRoJob], bool):
     jobs = []
     for job in job_list:
         title = job.select_one("div.job_header div.job_header_title h3").text.strip()
-        location, time_posted = map(str.strip, job.select_one("div.job_header div.job_header_title strong").text.split("|"))
+        tup = list(map(str.strip, job.select_one("div.job_header div.job_header_title strong").text.split("|")))
+        location, time_posted = None, None
+        if len(tup) == 3:
+            location = tup[1]
+            time_posted = tup[2]
+        elif len(tup) == 2:
+            location = tup[0]
+            time_posted = tup[1]
+
         tags = []
         for tag in job.select("ul.job_tags li a"):
             tags.append(tag.text)
 
         company = job.select_one("div.job_content div.d-flex ul.job_requirements li").text.split(":")[1].strip()
-        jobs.append(JuniorsRoJob(title, location, datetime.now() - approximate_time(time_posted), tags, company))
+        jobs.append(JuniorsRoJob(title, location, approximate_time(time_posted) or time_posted, tags, company))
 
     last_page = 0
     for page in b.select("ul.pagination li.page-item a"):
@@ -120,9 +134,65 @@ def parse_html(html: str) -> (list[JuniorsRoJob], bool):
         return (jobs, 1, 1)
 
 if __name__ == "__main__":
+    parser = ArgumentParser(prog="job_finder", description="finds jobs!")
+    parser.add_argument("search_text", help="The keyword to search on juniors.ro")
+    parser.add_argument("--job-types", nargs="+", help="The type of job to look for")
+    parser.add_argument("--remote", action="store_true", help="Whether the job should be remote")
+    parser.add_argument("--experience", nargs="+", help="list of numbers: 0 - no experience; 1 - less than a year; 2 - more than a year")
+    parser.add_argument("--last-days", help="Shows only offers posted in the last specified number of days")
+    args = parser.parse_args()
+
+    job_types = []
+    if args.job_types:
+        for job_type in args.job_types:
+            for ty in JuniorsRoJobType:
+                if ty.name.lower() == job_type.lower():
+                    job_types.append(ty)
+                    break
+            else:
+                print("Invalid type of job")
+                exit(-1)
+    else:
+        job_types = None
+
+    remote = JuniorsRoRemote.Remote if args.remote else JuniorsRoRemote.DontCare
+
+    experiences = []
+    if args.experience:
+        for experience in args.experience:
+            if experience == "0":
+                experiences.append(JuniorsRoExperience.NoExperience)
+            elif experience == "1":
+                experiences.append(JuniorsRoExperience.ZeroTo1Year)
+            elif experience == "2":
+                experiences.append(JuniorsRoExperience.MoreThanAYear)
+            else:
+                print("Unknown experience")
+                exit(-1)
+    else:
+        experiences = None
+
+    date = None
+    if args.last_days:
+        try:
+            days = int(args.last_days)
+            if days <= 1:
+                date = JuniorsRoDate.Last24h
+            elif days <= 3:
+                date = JuniorsRoDate.Last3Days
+            elif days <= 7:
+                date = JuniorsRoDate.Last7Days
+            elif days <= 31:
+                date = JuniorsRoDate.LastMonth
+            else:
+                date = None
+        except ValueError:
+            print("Expected number for --last-days")
+            exit(-1)
+
     page_num = 1
     while True:
-        url = make_url(search_query="", page=page_num)
+        url = make_url(search_query=args.search_text, job_types=job_types, experience=experiences, date=date, remote=remote, page=page_num)
         print(f"Url: {url}")
         page = requests.get(url)
         print(f"Status: {page}")
